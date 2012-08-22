301997bed95a9196b2437f60fb82e967b46f2928
Jiacong Zhong <zhongjiacong@gmail.com>
2012-08-23 06:36:17 +0800

    mysql

--- protected/msg.php
+++ protected/msg.php
@@ -1,6 +1,10 @@
 <?php
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
 		mysql_connect("localhost","zjc","dbconn743215968");
+		mysql_select_db("blog");
+		/*$query = "SELECT * FROM `msg`";
+		$result = mysql_query($query);*/
+		$query = "INSERT INTO `msg` ()";
 		//isset($_POST["content"])) {
 		echo json_encode(array(
 			"state"=>"succeed",
