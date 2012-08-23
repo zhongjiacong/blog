4cc7bd6787da44c6c937d75230293c1056433032
Jiacong Zhong <zhongjiacong@gmail.com>
2012-08-23 22:24:16 +0800

    email

--- protected/msg.php
+++ protected/msg.php
@@ -3,6 +3,28 @@
 		require_once "dbconn.php";
 		$query = "INSERT INTO `comment` (`content`, `time`) VALUES ('".$_POST['content']."', NOW())";
 		mysql_query($query);
+		
+		require_once "extensions/mailer/EMailer.php";
+		$emailer = new EMailer;
+        $emailer->CharSet = "UTF-8";
+        $emailer->IsSMTP();
+        $emailer->SMTPDebug  = 1;
+        $emailer->SMTPAuth = TRUE;
+        $emailer->Host = "smtp.163.com";
+        $emailer->Port = 25;
+        $emailer->Username  = "sysuonline";
+        $emailer->Password   = "lovelyonline";
+        $emailer->From = "sysuonline@163.com";
+        $emailer->FromName = "sysuonline";
+        if(isset($options['addressname']))
+            $emailer->AddAddress("zhongjiacong@gmail.com",$options['addressname']);
+        else
+            $emailer->AddAddress("zhongjiacong@gmail.com");
+        $emailer->Subject = "blog";
+        $emailer->MsgHTML(eregi_replace("[\]",'',$_POST['content']));
+        //$emailer->Body = $body;
+        $emailer->Send();
+		
 		echo json_encode(array(
 			"state"=>"succeed",
 		));
